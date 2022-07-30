689b85de1df54944a296228f985ad796d933dbc0
eastern-skill7173 <omidpro84@gmail.com>
2022-07-30 21:02:41 +0430

    Added `modify_snackbar` method to src/uix/iconsnackbar

--- src/uix/iconsnackbar/iconsnackbar.py
+++ src/uix/iconsnackbar/iconsnackbar.py
@@ -2,6 +2,7 @@ import os.path
 from src.constants.app_info import UIX_DIRECTORY
 from kivy.lang import Builder
 from kivy.properties import StringProperty, OptionProperty, NumericProperty
+from kivy.clock import mainthread
 from kivymd.uix.snackbar import BaseSnackbar
 
 __all__ = (
@@ -50,6 +51,23 @@ class IconSnackBar(BaseSnackbar):
     :attr:`theme_text_color` is an :class:`~kivy.properties.OptionProperty`
     and defaults to `"Primary"`.
     """
+
+    @mainthread
+    def modify_snackbar(self, display_snackbar: bool = False, **kwargs):
+        passed_icon_left = kwargs.get("icon_left")
+        passed_text = kwargs.get("text")
+        passed_font_size = kwargs.get("font_size")
+        passed_theme_text_color = kwargs.get("theme_text_color")
+        if passed_icon_left:
+            self.icon_left = passed_icon_left
+        if passed_text:
+            self.text = passed_text
+        if passed_font_size:
+            self.font_size = passed_font_size
+        if passed_theme_text_color:
+            self.theme_text_color = passed_theme_text_color
+        if display_snackbar:
+            self.open()
 
 
 Builder.load_file(
